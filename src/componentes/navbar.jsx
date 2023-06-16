1004418429f0742e2c6b717d7361152e63a36a70
unknown <everton6943@gmail.com>
2023-06-16 15:59:28 -0300

    alterações

--- src/componentes/navbar.jsx
+++ src/componentes/navbar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg" className='navbar-container'>
       <Navbar.Brand className='container-logo'  href="#home" ><a href="">BALANCE</a></Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Toggle className='icon-menu' aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto navbar-links">
           <Nav.Link className='link' href="#home" >HOME</Nav.Link>
